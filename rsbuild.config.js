05e79ebed3cffc860c1059165310848c2626386f
alexyz-kun <alexyz.primary@gmail.com>
2026-06-23 23:40:35 +0700

    Adjusted scripts in package.json

--- rsbuild.config.js
+++ rsbuild.config.js
@@ -4,5 +4,8 @@ import { pluginReact } from '@rsbuild/plugin-react';
 
 // Docs: https://rsbuild.rs/config/
 export default defineConfig({
-  plugins: [pluginReact()]
+  plugins: [pluginReact()],
+  output: {
+    assetPrefix: '/fun/',
+  },
 });
