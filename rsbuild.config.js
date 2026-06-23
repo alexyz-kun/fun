50ac24504ae4d01ae7ddf1e3ee21653cc2fd7b3f
alexyz-kun <alexyz.primary@gmail.com>
2026-06-23 23:26:19 +0700

    Fixed erroneous asset prefix

--- rsbuild.config.js
+++ rsbuild.config.js
@@ -4,8 +4,5 @@ import { pluginReact } from '@rsbuild/plugin-react';
 
 // Docs: https://rsbuild.rs/config/
 export default defineConfig({
-  plugins: [pluginReact()],
-  output: {
-    assetPrefix: '/fun/',
-  },
+  plugins: [pluginReact()]
 });
